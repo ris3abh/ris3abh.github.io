e6ac766757c97487d13fbf724f83d10b299202d6
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2023-04-02 17:58:25 -0400

    first push

---
+++ src/content_option.js
@@ -0,0 +1,180 @@
+const logotext = "Rishabh";
+const meta = {
+    title: "Rishabh Sharma",
+    description: "Hi there! My name is Rishabh, and I’m a data scientist with a passion for deep learning and big data.",
+};
+
+const introdata = {
+    title: "I’m Rishabh Sharma",
+    animated: {
+        first: "I love coding",
+        second: "I am a data scientist",
+        third: "I develop machine learning models and web apps",
+    },
+    description: "Hi there! My name is Rishabh, and I’m a data scientist with a passion for deep learning and big data.",
+    your_img_url: "https://animesher.com/orig/1/194/1943/19438/animesher.com_anime-aesthetic-cyberpunk-aesthetic-1943837.gif",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "Wassup!!",
+    aboutme1: " Hi there! My name is Rishabh, and I’m a data scientist with a passion for deep learning and big data.",
+    aboutme2: " I have experience using tools like TensorFlow, PyTorch, and Apache Spark to build models that can tackle complex problems. When I’m not tinkering with algorithms or poring over datasets, you can find me playing video games, binge watching animes, or trying out the latest tech gadgets. I’m always on the hunt for new ways to stay ahead of the curve, whether that means experimenting with new machine-learning models or trying out the latest programming language or something else entirely. Recently, I’ve been exploring the potential of deep learning to improve natural language processing and computer vision applications. I’ve also been experimenting with different approaches to handling big data, including distributed computing and cloud-based solutions.",
+    aboutme3: "So if you’re looking for someone who’s equal parts nerdy and fun, then look no further than Rishabh, your friendly neighborhood data scientist!! I’m currently looking for a full-time position as a data scientist.",
+};
+const worktimeline = [{
+        jobtitle: "Data Science Intern",
+        where: "Digital Pass",
+        date: "Feb - 2021 to Oct - 2021",
+        description1: "●Implemented unsupervised machine learning algorithms and conducted sentiment analysis of 5TB of unstructured text data collected from Twitter and feedback handles of the company utilizing Python programming language.",
+        description2:"●Worked on annotating a massive corpus of texts by utilizing Pysparks and various ML services with cloud computing resources from Google Cloud and AWS.",
+        description3: "●Constructed recommendation systems and chatbots for numerous companies while optimizing data pipelines for large-scale nearest-neighbor search algorithms",
+    },
+    {
+        jobtitle: "Data Science Intern",
+        where: "SK Enterprises",
+        date: "Nov - 2020 to Jan - 2021",
+        description1: "●Formulated statistical models to enhance fare products and overall customer experience by gaining insights into customers",
+        description2: "●Developed a predictive system to monitor machine performance and downtime with a 94% accuracy rate, leading to a 14% increase in overall equipment effectiveness(OEE)",
+    },
+    {
+        jobtitle: "SDE Intern",
+        where: "Oyelazy",
+        date: "Aug - 2020 to Oct - 2020",
+        description1: "●Developed a web application for a startup using React JS and Node JS",
+        description2: "● Developed a backend API for the web application using Node JS and MongoDB",
+        description3: "●Improved the performance of the web application by 20% by optimizing the code and using caching techniques",
+    },
+    {
+        jobtitle: "SDE Intern",
+        where: "Resillience",
+        date: "Jan - 2020 to Jul - 2020",
+        description1: "●Developed a web application for an educational firm using React JS and Node JS",
+        description2: "●Developed an interface for live video streaming using WebRTC and a live one-to-one video feature for doubt clearing sessions",
+        description3: "●Developed a backend API for the web application using Node JS and MongoDB",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 100,
+    },
+    {
+        name: "R",
+        value: 90,
+    },
+    {
+        name: "Machine Learning",
+        value: 100,
+    },
+    {
+        name: "Deep Learning",
+        value: 90,
+    },
+    {
+        name: "Hadoop",
+        value: 85,
+    },
+    {
+        name: "Pysparks",
+        value: 100,
+    },
+    {
+        name: "Data Science",
+        value: 100,
+    },
+    {
+        name: "Data Engineering",
+        value: 90,
+    },
+    {
+        name: "Data Mining",
+        value: 90,
+    },
+    {
+        name: "Natural Language Processing",
+        value: 90,
+    },
+    {
+        name: "Data Modeling",
+        value: 90,
+    },
+    {
+        name: "Data Architecture",
+        value: 90,
+    },
+    {   
+        name: "React JS",
+        value: 80,
+    },
+    {
+        name: "Node JS",
+        value: 80,
+    },
+
+];
+
+const dataportfolio = [{
+        img: "https://storage.googleapis.com/plos-corpus-prod/10.1371/journal.pone.0187336/1/pone.0187336.g001.PNG_L?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=wombat-sa%40plos-prod.iam.gserviceaccount.com%2F20230401%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230401T221540Z&X-Goog-Expires=86400&X-Goog-SignedHeaders=host&X-Goog-Signature=671582602955a40ac3750f53c3a53581713883e3485ac8962cb7b7ff60164357445e8da737eb8f0bcd5166d349f5c8a1ba050939874b3d633cd1cf17cf6f4cefd62cdbd7d034cd096835c1555bf45a5b3ee16b22396d8a2799623bdd5d65124b168f237dbeb0bc08e9741aabcbc34968ca4817625bdc535ad3260c3da92bbbe33aa5d48b9a20ac66b6c0d81291f686bf50d9401faba36201cf879e99ef2c476c0fd18e859f1e6e0851d373ce6b70f155eed885c36cf5a6708808e272538d9636dd5bbae15a0db508bd356dea7971b977223f47a3484811887c62cbb06695ddf54679780cb74653e25608f09512b2e65275cfdcbcfcf005be51b50de8928f4a07",
+        description: "Deep Learning Model for Multi-labeled classification of Diseases in Fundus Images.",
+        link: "https://github.com/ris3abh/rfmd",
+    },
+    {
+        img: "https://www.centercityphila.org/uploads/attachments/cleis8iq20uwisaqdfj9dhnrg-point-breeze-aerial-photography-wefilmphilly-1-1.635.821.4168.2605.full.jpg",
+        description: "ML to predict type of sentencing. Currently this project is used by the Thomas R. Kline School of Law at Drexel University.",
+        link: "https://github.com/ris3abh/Sentencing-prediction-on-docket-data",
+    },
+    {
+        img: "https://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/uploads/images/life-poster-2.jpg?itok=j4JGdHq0&c=e20fdff20cc5e1bb75d349713378a2e6",
+        description: "Impact and Sentiment analysis of Scientific Films",
+        link: "https://github.com/ris3abh/impact-sentiment-Analysis-Of-Scientific-Films",
+    },
+    {
+        img: "https://media.tenor.com/aVlSy7iGN8IAAAAC/groovin-music.gif",
+        description: "This is a project where I am trying to train a Generative Adversarial Network. This is my current on going project.",
+        link: "https://github.com/ris3abh/lyrics-generator",
+    },
+    {
+        img: "https://www.cnet.com/a/img/resize/b20f2b84030420ff9152e25dabd8be35d1b574b8/hub/2014/08/01/7fee1a9f-0bce-4317-b839-7f315e3bcfd8/flappy-birds-family.png?auto=webp&width=1200",
+        description: "The Flappy Birds.",
+        link: "https://github.com/ris3abh/FlappyBirds",
+    },
+    {
+        img: "https://raw.githubusercontent.com/ris3abh/DSCI521Covid-19DashBoard/main/images/forecast_confirmed.png",
+        description: "Covid-19 Time Series Forecasting and Death Prediction in Mexico.",
+        link: "https://github.com/ris3abh/DSCI521Covid-19DashBoard",
+    },
+
+    {
+        img: "https://pbs.twimg.com/media/ERUzNkZUcAAf3It.jpg:large",
+        description: "Solving Complex Mathematical Equations using Deep Learning.",
+        link: "https://github.com/ris3abh/Solving_math",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rishabh.sharma1103@gmail.com",
+    YOUR_FONE: "+1-(215)-966-7407",
+    description: "I’m currently looking for a full-time position as a data scientist. If you’re interested in working with me, then please feel free to reach out",
+    YOUR_SERVICE_ID: "service_70d0zad",
+    YOUR_TEMPLATE_ID: "template_tu0xodc",
+    YOUR_USER_ID: "gop9oZUBzzmNNxi4Z",
+};
+
+const socialprofils = {
+    github: "https://github.com/ris3abh",
+    facebook: "/",
+    linkedin: "https://www.linkedin.com/in/rishabh-sharma-06b4a3188/",
+    twitter: "https://twitter.com/rishabhx11",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/about/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  dataabout,
+  meta,
+  worktimeline,
+  skills,
+} from "../../content_option";
+
+export const About = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> About | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">About me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">{dataabout.title}</h3>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <div>
+              <p>{dataabout.aboutme}</p>
+              <p>{dataabout.aboutme1}</p>
+              <p>{dataabout.aboutme2}</p>
+              <p>{dataabout.aboutme3}</p>
+            </div>
+          </Col>
+        </Row>
+        <Row className=" sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Work Timline</h3>
+          </Col>
+          <Col lg="7">
+            {worktimeline.map((data, i) => {
+              return (
+                <div className="card" key={i}>
+                  <div className="card-body">
+                    <h5 className="card-title">{data.jobtitle}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {data.where}
+                    </h6>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {data.date}
+                    </h6>
+                    <p className="card-text">{data.description1}</p>
+                    <p className="card-text">{data.description2}</p>
+                    <p className="card-text">{data.description3}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Skills</h3>
+          </Col>
+          <Col lg="7">
+            {skills.map((data, i) => {
+              return (
+                <div key={i}>
+                  <h3 className="progress-title">{data.name}</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      style={{
+                        width: `${data.value}%`,
+                      }}
+                    >
+                      <div className="progress-value">{data.value}%</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </HelmetProvider>
+  );
+};
